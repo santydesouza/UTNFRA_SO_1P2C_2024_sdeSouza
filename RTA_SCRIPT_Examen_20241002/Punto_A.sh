bd4d1c9ee295bda340bae479b71919e6d6f7cd90
santydesouza <santydesouza4@gmail.com>
2024-10-06 15:09:00 +0000

    ADD: Despues de muchos muchos muchos videos, pusheamos el punto 1

---
+++ RTA_SCRIPT_Examen_20241002/Punto_A.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "creamos los grupos de cada parte"
+
+mkdir -p $HOME/Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
+
+echo "Presentamos al estructura"
+
+tree $home/vagrant/Examenes-UTN
+
+
